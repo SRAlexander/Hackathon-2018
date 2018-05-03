015f04b86237d70c1f6e4ccf21b93c8a90e949a4
Matt Cantillon <matthew.cantillon@gmail.com>
2018-05-03 10:45:40 +0100

    Adds view for search results
    Adds route for search results view

--- Front/dragnet/src/app/app-routing.module.ts
+++ Front/dragnet/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DetailsComponent } from './pages/search/details/details.component';
+import { ResultsComponent } from './pages/search/results/results.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'detailsSearch', component: DetailsComponent}
+  { path: 'detailsSearch', component: DetailsComponent},
+  { path: 'searchResults', component: ResultsComponent},
 ];
 
 @NgModule({
