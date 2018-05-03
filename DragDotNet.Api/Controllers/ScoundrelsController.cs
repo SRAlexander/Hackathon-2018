09c3eb2cc78d810176ad43cf9b50804c9440c374
David Banks <BanksySan@googlemail.com>
2018-05-03 11:21:59 +0100

    Oops

--- DragDotNet.Api/Controllers/ScoundrelsController.cs
+++ DragDotNet.Api/Controllers/ScoundrelsController.cs
@@ -21,13 +21,4 @@
             return Ok(new { CurrentUser.Username, Scoundrels = scoundrels});
         }
     }
-
-    public class FindScoundrelModel
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string DateOfBirth { get; set; }
-        public string Gender { get; set; }
-        public string Postcode { get; set; }
-    }
 }
--- DragDotNet.Api/PoliceApiClient.cs
+++ DragDotNet.Api/PoliceApiClient.cs
@@ -42,12 +42,4 @@
             }
         }
     }
-
-    public class ScoundrelInfo
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Gender { get; set; }
-        public string DoB { get; set; }
-    }
 }
